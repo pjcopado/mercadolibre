b595200afd8a3aa8c5be9ef3e9b336326f541a9f
Patricio Copado <pjcopado@gmail.com>
2023-03-06 18:15:12 +0100

    ADDED: test get user information endpoint

---
+++ test/user.py
@@ -0,0 +1,25 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+from meli.api.user import User
+from meli.base.marketplaces import Marketplaces
+
+CLIENT_ID = os.environ.get("MELI_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("MELI_CLIENT_SECRET")
+REFRESH_TOKEN = os.environ.get("MELI_REFRESH_TOKEN")
+
+
+CREDENTIALS = {
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
+    "refresh_token": REFRESH_TOKEN,
+}
+
+print(CREDENTIALS)
+
+res = User(credentials=CREDENTIALS, marketplace=Marketplaces.MLA)
+print(res)
+
+user = res.get_user_by_id(user_id="829069618")
+print(user)
